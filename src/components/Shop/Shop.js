185745247d8496a2722f5e88c40007412bec878c
kito078 <101482292+kito078@users.noreply.github.com>
2023-06-06 12:25:02 +0200

    shop update

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useEffect } from "react";
 import AvailableShop from "./AvailableShop";
 import Saver from "../Saver/Saver";
 
 function Shop(props) {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
   const productClickHandler = (product) => {
     props.onSaveProduct(product);
   };
