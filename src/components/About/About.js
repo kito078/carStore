50c6d68d615d41b526af3348a62b123a176b8615
kito078 <101482292+kito078@users.noreply.github.com>
2023-06-07 05:44:14 +0200

    about update

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -3,6 +3,7 @@ import "./About.css";
 import Feature2 from "../Feature/Feature";
 import FeatureOne from "../FeatureOne/FeatureOne";
 import Team from "../Team/Team";
+import About2 from "../../images/abt.jpg";
 
 function About() {
   return (
@@ -13,8 +14,23 @@ function About() {
           <h5>About Details About Details Right Sidebar</h5>
         </div>
       </div>
-
       <div className="container">
+        <div className="about__body ">
+          <div className="about__image">
+            <img src={About2} alt="Image" />
+          </div>
+          <div className="about__text">
+            <h2>We Are Always Ready To Assist Our Clients</h2>
+            <p>
+              Automotive’s four Frederick locations continues its 20++ plus
+              years tradition of quality auto repair services. ehicle owners in
+              the ederick, MD area should be well aware of the Dynamic
+              Automotive brand of service. We strive to go above and beyond for
+              all of our customers, who we pair services. ehicle owners.
+            </p>
+          </div>
+        </div>
+
         <Feature2 />
         <Team />
       </div>
